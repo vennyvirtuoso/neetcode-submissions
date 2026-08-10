b24a62e0f3a3a49641a5403c959068733cf27873
Vijendra Kumar Vaishya <78156586+vennyvirtuoso@users.noreply.github.com>
2026-08-10 06:27:00 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -0,0 +1,30 @@
+
+def quicksort(low,high,nums):
+    if low<high:
+        pivot = nums[((low+high)//2)]
+        i=low
+        j=high
+        while i<=j:
+            while nums[i]<pivot:
+                i=i+1
+            while nums[j]>pivot:
+                j=j-1
+            if i<=j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i=i+1
+                j=j-1
+
+        quicksort(low,j,nums)
+        quicksort(i,high,nums)
+    else:
+        return   
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        quicksort(0,n-1,nums)
+        return nums
+        
