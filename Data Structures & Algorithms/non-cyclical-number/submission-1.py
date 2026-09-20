b38f6da9d0ae5db7e09a3d1e8db34d0cba7aa9f0
Vijendra Kumar Vaishya <78156586+vennyvirtuoso@users.noreply.github.com>
2026-09-21 04:24:33 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        num=str(n)
+        sum=0
+        loop=100
+        while sum!=1 and loop:
+            sum=0
+            # print(sum)
+            for i in range(len(num)):
+                sum+=(int(num[i])**2)
+            num=str(sum)
+            loop-=1
+        return True and bool(loop)
+
+        
